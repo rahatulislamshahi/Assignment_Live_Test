901b1d646c63b7f8d1ddb31a872e707188f7ccd4
rahatulislamshahi <rahatulislamshahi123@gmail.com>
2025-04-06 22:30:25 +0600

    Initial commit

--- Assignment 1.dart
+++ Assignment 1.dart
@@ -1,30 +1,23 @@
-// Abstract class (blueprint for vehicles)
+
 abstract class Vehicle {
-  int _speed = 0; // Private speed variable (encapsulation)
+  int _speed = 0;
 
-  // Abstract method (must be implemented by subclasses)
-  void move();
-
-  // Method to set speed
   void setSpeed(int speed) {
     _speed = speed;
+    print("Speed Is: $_speed");
   }
 
-  // Getter method to safely access speed
-  int get speed => _speed;
+  void move();
 }
 
-// Car class (inherits from Vehicle)
 class Car extends Vehicle {
-  @override
   void move() {
-    print("The car is moving at $speed km/h");
+    print("The car is moving at $_speed km/h");
   }
 }
 
-// Main function (where we actually use the Car class)
 void main() {
-  Car myCar = Car(); // Creating a car object
-  myCar.setSpeed(80); // Setting the car's speed
-  myCar.move(); // Calling move() to print the speed
+  Car myCar = Car();
+  myCar.setSpeed(69);
+  myCar.move();
 }
